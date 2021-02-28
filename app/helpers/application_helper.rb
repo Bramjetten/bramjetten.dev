b1983a528a69b0e58c333e43c3bdd655b149a29b
Bram Jetten <mail@bramjetten.nl>
2021-02-28 10:01:02 +0100

    Spina update

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,13 @@
+module ApplicationHelper
+  def articles
+    @articles ||= Spina::Resource.find_by(name: "articles").pages.live
+  end
+  
+  def nav_items
+    @nav_items ||= Spina::Navigation.find_by(name: "main").navigation_items.roots.sorted
+  end
+  
+  def tags
+    @tags ||= Spina::Current.page.content(:tags).to_s.split(";").map(&:strip).compact
+  end
+end
