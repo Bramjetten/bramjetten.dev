22552e58298ae6c4331411f7188721cb8cbaf99d
Bram Jetten <mail@bramjetten.nl>
2021-06-29 10:20:20 +0200

    Images helper

--- app/helpers/spina/images_helper.rb
+++ app/helpers/spina/images_helper.rb
@@ -1,43 +1,43 @@
 module Spina
-module ImagesHelper
-
-  def thumbnail_url(image)
-    return "" if image.nil?
-    main_app.url_for(image.variant(format: :webp, saver: {quality: 70}, resize_to_fill: [400, 300]))
-  end
-
-  def embedded_image_url(image)
-    return "" if image.nil?
-
-    # support both ImageProcessing gem macro :resize_to_limit and ImageMagick-specific :resize
-    resize_key = if !Spina.config.embedded_image_size.is_a?(Array)
-                   :resize
-                 else
-                   :resize_to_limit
-                 end
-    main_app.url_for(image.variant(format: :webp, saver: {quality: 70}, Hash[resize_key, Spina.config.embedded_image_size]))
-  end
+  module ImagesHelper
   
-  def content_type_color(image)
-    case content_type(image)
-    when "png"
-      "bg-green-300"
-    when "heic"
-      "bg-blue-200"
-    when "jpg", "jpeg"
-      "bg-blue-400"
-    when "gif"
-      "bg-indigo-300"
-    when "svg"
-      "bg-yellow-400"
-    else
-      "bg-gray-400"
+    def thumbnail_url(image)
+      return "" if image.nil?
+      main_app.url_for(image.variant(format: :webp, saver: {quality: 70}, resize_to_fill: [400, 300]))
     end
-  end
   
-  def content_type(image)
-    image.file.content_type.split("/").last
+    def embedded_image_url(image)
+      return "" if image.nil?
+  
+      # support both ImageProcessing gem macro :resize_to_limit and ImageMagick-specific :resize
+      resize_key = if !Spina.config.embedded_image_size.is_a?(Array)
+                     :resize
+                   else
+                     :resize_to_limit
+                   end
+      main_app.url_for(image.variant(format: :webp, saver: {quality: 70}, Hash[resize_key, Spina.config.embedded_image_size]))
+    end
+    
+    def content_type_color(image)
+      case content_type(image)
+      when "png"
+        "bg-green-300"
+      when "heic"
+        "bg-blue-200"
+      when "jpg", "jpeg"
+        "bg-blue-400"
+      when "gif"
+        "bg-indigo-300"
+      when "svg"
+        "bg-yellow-400"
+      else
+        "bg-gray-400"
+      end
+    end
+    
+    def content_type(image)
+      image.file.content_type.split("/").last
+    end
+  
   end
-
-end
 end
